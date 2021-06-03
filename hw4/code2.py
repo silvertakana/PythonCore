d7030f371420d345e2d8dc5640b37c1cc10ecb3b
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-06-03 16:54:36 +1200

    Create code2.py

---
+++ hw4/code2.py
@@ -0,0 +1,15 @@
+import math
+a = float(input("a>>"))
+b = float(input("b>>"))
+c = float(input("c>>"))
+
+if a > 0 and b > 0 and c > 0 :
+    print("number accepted")
+    isposible = (a+b)>c and (a+c)>b and (c+b)>a
+    if isposible :
+        word = "possible"
+    else :
+        word = "not posible"
+    print(f"this triangle is {word}")
+else :
+    print("number can't be negative!")

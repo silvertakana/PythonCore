5dfe69c7a79f06015b9e7ad51fd746753a706315
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-06-12 13:09:58 +1200

    Update code6.py

--- hw4/code6.py
+++ hw4/code6.py
@@ -1,1 +1,10 @@
+n = float(input("n>>"))
+while round(n) != n:
+    print("n is not a natural number. try again")
+    n = float(input("n>>"))
+n = int(n)
 
+for i in range(n+1):
+    for j in range(i):
+        print(i,end="  ")
+    print()

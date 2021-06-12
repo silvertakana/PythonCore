1b14c26f83312c78bac44098dad878fc66bc24ec
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-06-12 12:43:35 +1200

    Update code4.py

--- hw4/code4.py
+++ hw4/code4.py
@@ -1,1 +1,12 @@
+n = float(input("n>>"))
+while round(n) != n:
+    print("n is not a natural number. try again")
+    n = float(input("n>>"))
+n = int(n)
 
+x=0
+for i in range(n+1):
+    x+=i
+
+z= (1+n)*(n)/2
+print(f"aswer: method1: {int(x)}; method2: {int(z)}")

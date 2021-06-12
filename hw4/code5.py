591d808ff266ae12fa4e054aac9de583f1199bcf
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-06-12 19:47:41 +1200

    Update code5.py

--- hw4/code5.py
+++ hw4/code5.py
@@ -1,14 +1,15 @@
+# get int x
 x = float(input("x>>"))
 while round(x) != x:
     print("x is not a natural number. try again")
     x = float(input("x>>"))
 x = int(x)
-
+# caculate
 n=0
 z=0
 while z<=x:
     n+=1
     z+=n
 n-=1
-
+#giving answer
 print(f"answer:{int(n)}")

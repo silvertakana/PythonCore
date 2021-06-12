3ea77259e142ac745c14094b973a2c6c31e49426
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-06-12 12:22:54 +1200

    Update code7.py

--- hw4/code7.py
+++ hw4/code7.py
@@ -1,1 +1,39 @@
+n = float(input("n>>"))
+while round(n) != n:
+    print("n is not a natural number. try again")
+    n = float(input("n>>"))
+        
 
+n = int(n)
+x = float(input("x>>"))
+
+S_1 = 0
+for i in range(n+1):
+    S_1+=x**i
+s1e = (1-x**(n+1))/ (1-x)
+print(f"S_1: method 1: {int(S_1)} ; method 2: {int(s1e)}")
+
+S_2 = 0
+for i in range(n+1):
+    S_2+=(-x)**i    
+
+s2e = (1-(-x)**(n+1))/ (1+x)
+print(f"S_2: method 1: {int(S_2)} ; method 2: {int(s2e)}")
+
+def fac(z):
+    y = 1
+    for i in range(1,z+1):
+        y*=i
+    return y
+
+S_3 = 0
+for i in range(n+1):
+    S_3+=(x**i)/ fac(i)
+
+
+s3e = 1
+a = 1
+for i in range(1,n+1):
+    a=  a*x/i
+    s3e = s3e+a
+print(f"S_3: method 1: {int(S_3)} ; method 2: {int(s3e)}")

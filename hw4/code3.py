55bca0464b12f4beb06a5812b1a228eb5d2bc01a
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-06-12 12:32:58 +1200

    Update code3.py

--- hw4/code3.py
+++ hw4/code3.py
@@ -1,1 +1,12 @@
+n = float(input("n>>"))
+while round(n) != n:
+    print("n is not a natural number. try again")
+    n = float(input("n>>"))
+n = int(n)
 
+i = 1
+x = 1
+while i*3<n:
+    x+=i*3
+    i+=1
+print(x)

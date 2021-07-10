ba7acfb8aa211deb207ca2c3278d7c51651fee2e
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-07-10 17:15:45 +1200

    Update code3.py

--- hw5/code3.py
+++ hw5/code3.py
@@ -1,8 +1,12 @@
 t = input("your text>>")
 min_char = t[0]
+max_char = t[0]
 
 for item in t:
-     if item<min_char:
-        min_char = item
+    if item<min_char:
+      min_char = item
+    if item>max_char:
+      max_char = item
 
-print(min_char)
+print(f"min_char: {min_char}")
+print(f"max_char: {max_char}")

bba7313fc17f2055d5c9b4ceaa15711770313bf3
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-07-15 20:55:19 +1200

    Update code8.py

--- hw6/code8.py
+++ hw6/code8.py
@@ -1,1 +1,13 @@
+def change_upper_lower1(str):
+    ns = ""
+    for i in range(len(str)):
+        s = str[i]
+        if(s.isupper()):
+            ns += str[i].lower()
+        else:
+            ns += str[i].upper()
+    return ns
 
+def change_upper_lower2(str):
+    return str.swapcase()
+print(change_upper_lower2("Hello World!"))

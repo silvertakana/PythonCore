aeede2208c3484c42545a542825564baad197851
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-07-16 10:32:21 +1200

    Update code12.py

--- hw6/code12.py
+++ hw6/code12.py
@@ -1,1 +1,7 @@
+width = int(input("width>> "))
+height = int(input("height>> "))
 
+def create_Matrix(width,height):
+  return [[x*y for x in range(width)] for y in range(height)] 
+
+print(create_Matrix(width,height))

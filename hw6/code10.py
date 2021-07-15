b9aa292ee6456ad6f4ce01ef12612ecb677923f2
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-07-15 21:03:22 +1200

    Update code10.py

--- hw6/code10.py
+++ hw6/code10.py
@@ -1,1 +1,12 @@
+def Tribonacci(n):
+    if(n == 1):
+        return 1
+    if(n == 2):
+        return 1
+    if(n == 3):
+        return 2
+    return Tribonacci(n-1) + Tribonacci(n-2) + Tribonacci(n-3)
 
+n = float(input("n>> "))
+
+print(f"Tribonacci of n = {Tribonacci(n)}")

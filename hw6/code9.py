f1ec9cd1e5ceb09caa8c3e2b91ce44844ada94a5
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-07-17 11:52:01 +1200

    Update code9.py

--- hw6/code9.py
+++ hw6/code9.py
@@ -1,1 +1,10 @@
+def count_odds(text,index = 0):
+    txt = str(text)
+    letter = txt[index]
+    num = int(letter)
+    i = index+1
+    if(i == len(txt)): return (num%2)
+    return count_odds(text,i) + (num%2)
 
+n = int(input("n>>"))
+print(f"the amount of odd digits in your number is: {count_odds(n)}")

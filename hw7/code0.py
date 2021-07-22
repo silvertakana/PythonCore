29f1f22343b3231fce2d287ef03c052bcb5c18ef
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-07-22 21:54:52 +1200

    Update code0.py

--- hw7/code0.py
+++ hw7/code0.py
@@ -3,10 +3,11 @@ def list_total(l):
     for x in l:
         y+=x
     return y
-    
+print(list_total([2,3,4]))
+
 def list_mult(l):
     y = 1
     for x in l:
         y*=x
     return y
-print(list_mult([3,7]))
+print(list_mult([2,3,4]))

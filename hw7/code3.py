f6e5551582850cac109b8e70000ff323182b1b92
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-07-22 22:48:17 +1200

    Create code3.py

---
+++ hw7/code3.py
@@ -0,0 +1,5 @@
+def strange_opperations_and_complex_matrix_math(l,s):
+    for x in range(len(l)):
+        l[x]+=s
+    return l
+print(strange_opperations_and_complex_matrix_math(["x","y","z"],"abc"))

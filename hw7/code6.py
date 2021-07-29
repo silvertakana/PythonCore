9eda830f4aac179ade87a84a450df697dee732e1
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-07-29 17:35:44 +1200

    Update code6.py

--- hw7/code6.py
+++ hw7/code6.py
@@ -1,8 +1,13 @@
 my_list = ["what","why","lol","o","wow"]
 
+# meadthod 1
 counter = 0
 
 for i in my_list:
     if len(i) >= 2 and i[0] ==i[-1]:
          counter += 1
 print(counter)
+
+# medthod 2
+
+print(len([i for i in my_list if len(i) >= 2 and i[0] ==i[-1]]))

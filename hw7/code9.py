f0d1bc792c8c48046a6c251471d69353f8842ddf
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-07-31 08:20:19 +1200

    Update code9.py

--- hw7/code9.py
+++ hw7/code9.py
@@ -1,1 +1,16 @@
+import json
 
+print("please enter your id in this format: [a,b,c,d,e,f,g,h,i,j,k,l...]")
+list_of_songs = input('all songs id>>')
+
+list_of_songs = json.loads(list_of_songs)
+all_songs = []
+for i in list_of_songs:
+    is_repeated = False
+    for j in all_songs:
+        if(i == j):
+            is_repeated = True
+            break
+    if(not is_repeated):
+        all_songs.append(i)
+print(all_songs)

b2cc031c0348bd7aaa0b471d8d7f5a8a2e943327
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-07-30 17:24:22 +1200

    Update code8.py

--- hw7/code8.py
+++ hw7/code8.py
@@ -1,1 +1,10 @@
+import random
+A = [random.randint(0,1e3) for i in range(0,10)]
 
+counter = 0
+for j in range(0,len(A)):
+    for i in range(0,len(A)):
+        if A[i] > A[j] and i < j:
+            counter += 1
+print(A)
+print(counter)

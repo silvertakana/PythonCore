4c177fc3be145149483b4ed85554c56248276afc
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-08-05 21:25:32 +1200

    Update code9.py

--- hw8/code9.py
+++ hw8/code9.py
@@ -1,1 +1,11 @@
+from random import uniform
 
+my_numbers = [uniform(0,10000000) for i in range(0,100)]
+my_numbers = tuple(my_numbers)
+print("Largest element is:", max(my_numbers))
+
+total = 0
+for i in range(len(my_numbers)):
+    total += my_numbers[i]
+
+print(total)

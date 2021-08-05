50e2ea0c1fcf3824a2a4d7a07ae69cb944f8005f
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-08-05 16:40:13 +1200

    Update code11.py

--- hw8/code11.py
+++ hw8/code11.py
@@ -1,1 +1,11 @@
+sentence = input("enter your sentence:\n")
 
+words = sentence.split(" ")
+
+longest_word = words[0]
+
+for i in words:
+    if(len(i) > len(longest_word)):
+        longest_word = i
+
+print(longest_word)

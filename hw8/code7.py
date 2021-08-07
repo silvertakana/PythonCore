f7363b65c7bcf3b7b2d6637946447031d248503a
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-08-07 16:47:26 +1200

    Update code7.py

--- hw8/code7.py
+++ hw8/code7.py
@@ -1,1 +1,15 @@
+from random import randint
 
+def is_colide(t1,t2):
+    for i in t1:
+        for j in t2:
+            if i == j: return True
+    return False
+
+my_tuple1 = tuple([randint(0,500) for i in range(0,randint(1,30))])
+my_tuple2 = tuple([randint(0,500) for i in range(0,randint(1,30))])
+
+print(my_tuple1)
+print(my_tuple2)
+
+print(is_colide(my_tuple1, my_tuple2))

1e8aee55e4a7a7279084f18fb7b842e6507104d9
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-08-07 15:28:32 +1200

    Update code3.py

--- hw8/code3.py
+++ hw8/code3.py
@@ -1,1 +1,14 @@
 
+mylist1 = ["apple", "banana", "cherry", "mango", "carrot", "peach"]
+mylist2 = ["apple", "banana", "cherry",("fish","pig","cow","sheep"), "mango", "carrot", "peach"]
+
+def a_weird_way_to_count(da_list):
+    size = 0
+    for i in da_list:
+        if(isinstance(i,tuple)):
+            break
+        size+=1
+    return size
+
+print(a_weird_way_to_count(mylist1))
+print(a_weird_way_to_count(mylist2))

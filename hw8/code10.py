ec2fa54515050a0459300096ad6cc0851e31da7a
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-08-07 15:08:31 +1200

    Update code10.py

--- hw8/code10.py
+++ hw8/code10.py
@@ -1,1 +1,6 @@
 
+my_websites = ["www.hust.edu.vn", "www.wikipedia.org", "www.asp.net", "www.amazon.com"]
+
+my_websites_suffix = [i.split(".")[-1] for i in my_websites]
+my_websites_suffix = tuple(my_websites_suffix)
+print(my_websites_suffix)

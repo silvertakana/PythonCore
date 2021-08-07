66d8c870f9c5d76d7a68e6c152133d9117d5f793
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-08-07 15:42:22 +1200

    Update code4.py

--- hw8/code4.py
+++ hw8/code4.py
@@ -1,1 +1,18 @@
 
+from random import randint
+
+# create the list
+the_list = []
+for i in range(randint(0,10)):
+    the_tuple = tuple([randint(0,500) for i in range(randint(1,4))])
+    the_list.append(the_tuple)
+
+# define sort function
+def sorter(t):
+    return t[-1]
+
+# sort list
+the_list.sort(key = sorter)
+
+# print list
+print(the_list)

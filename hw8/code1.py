60353a4aef4ba890b7374e50cf3ae330079df7c3
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-08-07 14:59:42 +1200

    Update code1.py

--- hw8/code1.py
+++ hw8/code1.py
@@ -1,1 +1,11 @@
 
+from random import uniform
+
+my_list = [uniform(0,10000000) for i in range(0,3)]
+my_tuple = tuple(my_list)
+
+my_2ndlist = list(my_tuple)
+
+print(my_list)
+print(my_tuple)
+print(my_2ndlist)

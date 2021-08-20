47f06d47b4c500f4540c8b00b312ba1e3cdd98ee
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-08-21 11:40:57 +1200

    Update code4.py

--- hw9/code4.py
+++ hw9/code4.py
@@ -1,1 +1,7 @@
+from random import randint
+my_dict = dict([(f"element {i}",[randint(0,9) for j in range(randint(5,10))]) for i in range(randint(3,10))])
 
+print(my_dict)
+
+for i in my_dict.values():
+	print(i[4])

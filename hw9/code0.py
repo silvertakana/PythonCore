c67516dc9834613746b61b4c735a57cd1912926a
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-08-21 10:49:42 +1200

    Create code0.py

---
+++ hw9/code0.py
@@ -0,0 +1,12 @@
+name_ids = {
+	"TAcHicepTioUsH": 1283,
+	"RtuatlEnthAINi": 1231,
+	"OTeaMiSetRIshi": 2712,
+	"dreMNAlEARACKL": 5629,
+	"SiTiTUstELeTIc": 8572
+}
+tích = 1
+for i in name_ids:
+	tích*=name_ids[i]
+
+print(tích)

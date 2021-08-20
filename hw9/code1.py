4b7f72d7c945a6209a4946a5929ae1f885af708c
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-08-21 10:58:06 +1200

    Update code1.py

--- hw9/code1.py
+++ hw9/code1.py
@@ -1,1 +1,16 @@
+import math
+name_ids = {
+	"TAcHicepTioUsH": 1283,
+	"RtuatlEnthAINi": 1231,
+	"OTeaMiSetRIshi": 2712,
+	"dreMNAlEARACKL": 5629,
+	"SiTiTUstELeTIc": 8572
+}
+minimum = float('inf')
+maximum = float('-inf')
+for i in name_ids:
+	n = name_ids[i]
+	if(n > maximum): maximum = n
+	if(n < minimum): minimum = n
 
+print(maximum,minimum)

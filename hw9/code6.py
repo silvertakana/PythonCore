7e8ea6905f9f35e299bd1b32c991d8726e2cf25f
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-08-21 12:24:40 +1200

    Update code6.py

--- hw9/code6.py
+++ hw9/code6.py
@@ -1,1 +1,6 @@
+sampleDict = {"name": "Kelly", "age":25, "salary": 8000, "city": "New york"}
+keys = ["name", "salary"]
 
+def get_keys(d,keys):
+	return dict([(i,d[i]) for i in keys])
+print(get_keys(sampleDict, keys))

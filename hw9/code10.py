293f520f38506fc26481856e7f00207ade596d33
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-08-21 13:31:52 +1200

    Update code10.py

--- hw9/code10.py
+++ hw9/code10.py
@@ -1,1 +1,9 @@
+the_object =  [{'item': 'item1', 'amount': 400}, {'item': 'item2', 'amount': 300}, {'item': 'item1', 'amount': 750}]
 
+a = []
+for i in the_object:
+	for j in i:
+		a.append((j,i[j]))
+print(a)
+
+print(dict(a))

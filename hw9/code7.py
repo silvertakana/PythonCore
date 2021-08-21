df798e2452ebd1601d3cbea7b3469cf4e6bd6f58
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-08-21 12:27:29 +1200

    Update code7.py

--- hw9/code7.py
+++ hw9/code7.py
@@ -1,1 +1,10 @@
-
+name_ids = {
+	"TAcHicepTioUsH": 1283,
+	"RtuatlEnthAINi": 1231,
+	"OTeaMiSetRIshi": 2712,
+	"dreMNAlEARACKL": 5629,
+	"SiTiTUstELeTIc": 8572
+}
+a = list(name_ids.values())
+a.sort()
+print(a[-3:])

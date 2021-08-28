4fec05e0a81d4a8391f3c1101243243055079f21
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-08-29 09:11:35 +1200

    Update code10.py

--- hw9/code10.py
+++ hw9/code10.py
@@ -3,7 +3,9 @@ the_object =  [{'item': 'item1', 'amount': 400}, {'item': 'item2', 'amount': 300
 a = {}
 
 for i in the_object:
-	a[i['item']] = 0
-	a[i['item']] += i['amount']
+	if(i['item'] in a):
+		a[i['item']] += i['amount']
+	else:
+		a[i['item']] = i['amount']
 
 print(a)

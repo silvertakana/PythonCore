0f18205c6281a21d83cc6f73326539438a544971
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-08-29 09:41:05 +1200

    Update code5.py

--- hw9/code5.py
+++ hw9/code5.py
@@ -1,12 +1,12 @@
 from random import randint
 
 def key_intersection(d1,d2):
-	joined_dict_keys = set(d1) & set(d2)
-	print(joined_dict_keys)
+	joined_dict_keys = set(d1.items()) & set(d2.items())
+	print(dict(joined_dict_keys))
 
 
-my_dict = dict([(f"{randint(0,10)}",randint(0,10000)) for i in range(randint(1,100))])
-my_dict2 = dict([(f"{randint(0,10)}",randint(0,10000)) for i in range(randint(1,100))])
+my_dict = dict([(f"{randint(0,10)}",randint(0,10)) for i in range(randint(50,200))])
+my_dict2 = dict([(f"{randint(0,10)}",randint(0,10)) for i in range(randint(50,200))])
 
 print(my_dict)
 print(my_dict2)

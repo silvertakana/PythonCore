4b6b2898b2c4f8b676a03d77f970d35e9d40b559
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-08-29 09:19:05 +1200

    Update code9.py

--- hw9/code9.py
+++ hw9/code9.py
@@ -1,6 +1,11 @@
 your_sentence = input("enter your sentence>> ")
+your_sentence_words = your_sentence.split(" ")
 
-words_list = your_sentence.split(" ")
+words_dict = {}
+for word in your_sentence_words:
+	if(word in words_dict):
+		words_dict[word]+= 1
+	else:
+		words_dict[word] = 1
 
-print(words_list)
-print(len(words_list))
+print(words_dict)

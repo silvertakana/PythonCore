5ff270a3a5100c90680c3b7836b202aea1179dc7
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-09-04 18:13:42 +1200

    Update code0.py

--- hw10/code0.py
+++ hw10/code0.py
@@ -1,1 +1,11 @@
+class player:
+	def __init__(self,name,id):
+		self.name = name
+		self.id = id
+	def generate_username(self):
+		return f"{self.name}#{self.id}"
 
+silver = player("silver", 69)
+
+print(silver.generate_username())
+print(type(silver))

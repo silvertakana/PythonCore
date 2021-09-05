4259d5ce0788a06f85a9dda9daaf5836eac08f52
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-09-05 20:08:52 +1200

    Update code5.py

--- hw10/code5.py
+++ hw10/code5.py
@@ -1,1 +1,9 @@
+class NewString:
+	def get_string(self, string):
+		self.string = string
+	def print_string(self):
+		print(self.string.upper())
 
+string = NewString()
+string.get_string(input("your string >>"))
+string.print_string()

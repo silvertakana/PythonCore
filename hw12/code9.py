ead251b0332283d917c382e91109efcd6e1c20f2
Silvertakana <62156548+silvertakana@users.noreply.github.com>
2021-09-25 15:16:30 +1200

    Update code9.py

--- hw12/code9.py
+++ hw12/code9.py
@@ -1,1 +1,10 @@
+def extract_characters(*files):
+	characters = ""
+	for file in files:
+		f = open(file, "r")
+		characters += f.read()
+		f.close()
+	return set(characters)
 
+
+print(extract_characters("mytext.txt", "mytext2.txt", "mytext3.txt"))
